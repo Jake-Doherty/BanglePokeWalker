665533b349f7bf2967f37ac8f965178284f47a0e
Jacob Doherty <doherty.jacob.p@gmail.com>
2025-12-29 12:05:22 -0800

    bpw_theme: export theme constants and palette as top-level exports

---
+++ app/bpw_theme.js
@@ -0,0 +1,22 @@
+(function (exports) {
+  exports.P_BG = "#9ca38a";
+  exports.P_FG = "#3d4235";
+  exports.palette = new Uint16Array([
+    0xc676, // #C4CFB4 - Background (Lightest)
+    0xb615, // #B8C3A9
+    0xad73, // #ABB79E
+    0xa552, // #A1AB93
+    0x8cd0, // #8E9983 - Mid-Tones
+    0x7c4e, // #7E8875
+    0x6bcd, // #6F7868
+    0x632b, // #60685A
+    0x52ca, // #51594D - Active Pixels
+    0x4248, // #444B41
+    0x31e6, // #373D35
+    0x2985, // #2B302A
+    0x1903, // #1E211D - The "Ink"
+    0x10c2, // #151814
+    0x0861, // #0D0F0D
+    0x0000, // #050505 - Absolute Black
+  ]);
+})(typeof exports !== "undefined" ? exports : (this.exports = {}));
