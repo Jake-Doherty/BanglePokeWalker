9979d5ac79d485f6aa0cf351db9a6ef980be3a2c
Jacob Doherty <doherty.jacob.p@gmail.com>
2025-12-29 12:04:53 -0800

    bpw_data: export top-level data arrays (menuItems, menuICONS, volLabels, contrLabels, routeNames); normalize module shape

---
+++ app/bpw_data.js
@@ -0,0 +1,62 @@
+(function (exports) {
+  exports.menuItems = [
+    "RADAR",
+    "DOWSING",
+    "CONNECT",
+    "STATS",
+    "INVENTORY",
+    "SETTINGS",
+  ];
+
+  exports.menuICONS = [
+    "Pokéradar",
+    "Dowsing_Mach",
+    "Connect",
+    "Trainer_Card",
+    "Pkmn_and_Itms",
+    "Settings",
+    "Route",
+    "Trainer",
+    "Pokémon",
+    "Item",
+  ];
+
+  exports.volLabels = [
+    "MUTE",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "MAX",
+  ];
+
+  exports.contrLabels = [
+    "MIN",
+    "20",
+    "25",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "MAX",
+  ];
+
+  exports.routeNames = [
+    "Forest2",
+    "Paths2",
+    "Caves2",
+    "City2",
+    "Plains2",
+    "Lakes2",
+    "Sea2",
+    "Towns2",
+  ];
+})(typeof exports !== "undefined" ? exports : (this.exports = {}));
