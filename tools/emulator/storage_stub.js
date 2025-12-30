20816b91275bd6a5bef24863ee9173b1f1c11b9f
Jacob Doherty <doherty.jacob.p@gmail.com>
2025-12-29 21:24:25 -0800

    emulator: changes storage directory used by the emulator

--- tools/emulator/storage_stub.js
+++ tools/emulator/storage_stub.js
@@ -1,31 +1,35 @@
 // Minimal Storage shim for Node-based smoke tests
-const path = require('path');
-const fs = require('fs');
-const STORAGE_DIR = path.resolve(__dirname, '../../tmp_emu_storage');
-if(!fs.existsSync(STORAGE_DIR)) fs.mkdirSync(STORAGE_DIR, { recursive: true });
+const path = require("path");
+const fs = require("fs");
+const STORAGE_DIR = path.resolve(__dirname, "./tmp_emu_storage");
+if (!fs.existsSync(STORAGE_DIR)) fs.mkdirSync(STORAGE_DIR, { recursive: true });
 
-exports.read = function(file){
+exports.read = function (file) {
   const p = path.join(STORAGE_DIR, file);
-  if(!fs.existsSync(p)) return undefined;
-  return fs.readFileSync(p, 'utf8');
+  if (!fs.existsSync(p)) return undefined;
+  return fs.readFileSync(p, "utf8");
 };
 
-exports.write = function(file, content){
+exports.write = function (file, content) {
   const p = path.join(STORAGE_DIR, file);
-  fs.writeFileSync(p, content, 'utf8');
+  fs.writeFileSync(p, content, "utf8");
 };
 
-exports.readJSON = function(file, rev){
+exports.readJSON = function (file, rev) {
   const c = exports.read(file);
-  if(!c) return undefined;
-  try{ return JSON.parse(c); }catch(e){ return undefined; }
+  if (!c) return undefined;
+  try {
+    return JSON.parse(c);
+  } catch (e) {
+    return undefined;
+  }
 };
 
-exports.writeJSON = function(file, obj){
+exports.writeJSON = function (file, obj) {
   exports.write(file, JSON.stringify(obj));
 };
 
-exports.list = function(){
+exports.list = function () {
   return fs.readdirSync(STORAGE_DIR);
 };
 
